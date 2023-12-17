bf1c2e64a676898638f387c754c0835607a582ea
Tim Jason Weber <timwweberj@gmail.com>
2023-12-17 01:33:45 +0100

    manual editing of .versiontool required

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <filesystem>
 #include <fstream>
+#include <cstdlib>
 
 class ConfigFileNames{
     private:
@@ -30,33 +31,37 @@ class ConfigFileNames{
 };
 
 int main(){ 
-    std::string a = "Hello";
-    std::cout << "This is a test " << a << "\n";
-    
+
     if(std::filesystem::exists(".versiontool")){
-        std::cout << "ConfigFile is present";
+        std::cout << "ConfigFile is present" << std::endl;
 
-        std::ifstream fin(".versiontool");
+        std::ifstream versiontoolFile(".versiontool");
 
-        ConfigFileNames *objects[50];
         int i = 0;
-        while(!fin.eof()){
+        std::string filearray[10];
+        while(!versiontoolFile.eof()){
             std::string storedFile;
             
-            fin >> storedFile;
-            
+            versiontoolFile >> storedFile;
+            filearray[i] = storedFile;
             std::cout << "storedFile: " << storedFile << std::endl;
-            
-            objects[i] = new ConfigFileNames(storedFile, i);
 
-            std::cout << "array object: " << objects[i] << std::endl;
-            i++;
+            ++i;
         }
-        std::cout << "Full array" << objects << std::endl;
+
+        for(int i = 0; i < sizeof(filearray) / sizeof(filearray[0]); i++){
+            std::cout << "Array Item: " << filearray[i] << std::endl;
+        }
     }
     else{
         std::cout << "ConfigFile is not present";
+        std::ofstream versiontoolFile(".versiontool"); // create file
+        
+        if(std::system("cmd /c code .versiontool") != 0){
+            std::system("cmd /c notepad .versiontool");
+        }
     }
+    
     return 0;
     // std::this_thread::sleep_for(std::chrono::seconds(2));
 }
